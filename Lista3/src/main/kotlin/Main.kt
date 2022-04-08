c398b8176e7a7f334b867db07e47a90f4b355bd3
Cielako <cielakes@gmail.com>
2022-04-08 18:11:46 +0200

    Lista 3 - start

---
+++ Lista3/src/main/kotlin/Main.kt
@@ -0,0 +1,52 @@
+import kotlin.random.*
+import java.io.File
+import java.io.BufferedReader
+fun main(args: Array<String>) {
+    var hangman_pics = listOf(
+        """
+     +---+
+         |
+         |
+         |
+       ===""",
+        """
+     +---+
+     o   |
+         |
+         |
+       ===""",
+        """
+     +---+
+     o   |
+     |   |
+         |
+       ===""",
+        """
+     +---+
+     o   |
+    /|   |
+         |
+       ===""",
+        """
+     +---+
+     o   |
+    /|\  |
+         |
+       ===""",
+        """
+     +---+
+     o   |
+    /|\  |
+    /    |
+       ===""",
+        """
+     +---+
+     o   |
+    /|\  |
+    / \  |
+       ===""")
+//hangman_pics.forEach{println(it)}4
+    val bufferedReader: BufferedReader = File("$path/slowa.txt").bufferedReader()
+    val inputString = bufferedReader.use { it.readText() }
+    println(inputString)
+}
