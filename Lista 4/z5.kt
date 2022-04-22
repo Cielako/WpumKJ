50b66aee5a4899ca46eebd8f320de4462ed43a4b
Cielako <cielakes@gmail.com>
2022-04-22 11:59:37 +0200

    Lista 4 - zad 5

--- Lista 4/z5.kt
+++ Lista 4/z5.kt
@@ -1,21 +1,23 @@
 fun log2(i: Int): Int{
     fun log2(i: Int): Int{
-        return (kotlin.math.log2(i.toDouble()).toInt())
+       return (kotlin.math.log2(i.toDouble()).toInt())
     }
     return log2(i)
-}
-
-fun fib(n: Int): Int{
-    tailrec fun fib(n: Int, a: Int = 0, b:Int = 1): Int { 
-        if(n == 0) return b
-        return  fib(n-1, b, a+b) 
+ }
+ 
+ fun fib(n: Int): Int{
+    tailrec fun fib(n: Int, a: Int = 0, b:Int = 1): Int {
+       if(n == 0) return b
+       return  fib(n-1, b, a+b)
     }
     // Obliczamy wynik za pomocą funkcji zagnieżdżonej
     return(fib(n-1))
-}
-
-fun format()
-
-fun main(){
-
-}
+ }
+ 
+ fun format(value: Int, func: (Int) -> Int) = println(func(value))
+ 
+ fun main(){
+    format(3,::log2)
+    format(3,::fib)
+ }
+ 
